d95c0afc67d4371d6c43102fbfbfe107fe05275a
Matheus Moreno <62890686+dormantbow@users.noreply.github.com>
2025-02-05 11:45:48 -0300

    Delete door_monitoring directory

--- door_monitoring/doors/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import DoorStatusView, AccessHistoryView, ScheduleView, UpdateDoorStatusView
-
-urlpatterns = [
-    path('status/', DoorStatusView.as_view(), name='door_status'),
-    path('history/', AccessHistoryView.as_view(), name='access_history'),
-    path('schedule/', ScheduleView.as_view(), name='door_schedule'),
-    path('update-status/', UpdateDoorStatusView.as_view(), name='update_door_status'),
-]
